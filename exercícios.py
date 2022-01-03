5e211ccfa89f8fce4f1e9d74b8585c98af30d0dd
Gustavo Saez Ferreira <hero_uzumaki@hotmail.com>
2022-01-03 14:03:08 -0300

    Update exercícios.py

--- exercícios.py
+++ exercícios.py
@@ -69,7 +69,7 @@ b) Podem existir dias sem faturamento, como nos finais de semana e feriados.
 Estes dias devem ser ignorados no cálculo da média;
 """
 
-valores_mensais = [10, 15, 20, 18, 17, 0, 0, 12, 13, 22, 19, 15, 0, 0, 17, 23, 11, 12, 9, 0, 10, 11, 17, 21, 17, 20, 0, 0, 14, 25]
+valores_mensais = [22174.1664, 24537.6698, 26139.6134, 0.0, 0.0, 26742.6612, 0.0, 42889.2258, 46251.174, 11191.4722, 0.0, 0.0, 3847.4823, 373.7838, 2659.7563, 48924.2448, 18419.2614, 0.0, 0.0, 35240.1826, 43829.1667, 18235.6852, 4355.0662, 13327.1025, 0.0, 0.0, 25681.8318, 1718.1221, 13220.495, 8414.61]
 
 maior = max(valores_mensais)
 print(f'O maior valor do mês foi de {maior}R$')
